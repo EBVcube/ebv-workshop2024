7dcae10d881b67b84f70a0888d1124e480450ff8
Luise Quoß <luise.quoss@posteo.de>
2024-10-09 11:10:54 +0200

    add tasks for handson

--- code/01_explore_dataset.R
+++ code/01_explore_dataset.R
@@ -1,9 +1,9 @@
-# load the ebvcube package
+# load the ebvcube and stringr package
 library(ebvcube)
 library(stringr)
 
-#Documentation----
-# Remember to check the help-pages of the function using ?
+# Documentation----
+# Remember to check the help-pages of the function, e.g. using ?
 ?ebv_download
 
 # 1. Download ----
@@ -11,26 +11,29 @@ library(stringr)
 downloads <- ebv_download()
 head(downloads)
 
-# Download the 'Global trends in biodiversity (BES-SIM PREDICTS)' dataset
-path <- ebv_download(id = '10.25829/bk5g87', #alternative value: downloads$id[18]
+# Download the 'Global trends in biodiversity (BES-SIM PREDICTS)' dataset using the DOI
+path <- ebv_download(id = '10.25829/bk5g87', #alternative: downloads$id[18]
                      outputdir = '../data/') 
 
 # 2. Explore available datacubes ----
 # Get info about cubes including the hierarchy
 cubes <- ebv_datacubepaths(filepath = path)
+
 # This dataset has three scenarios, with four metrics
 print(cubes)
 
 # 3. Explore the metadata ----
 prop <- ebv_properties(filepath = path)
 
-# Include specific info for one metric and scenario, e.g. of the relative species change of scenario 1
+# Include specific info for one metric and scenario, e.g. of the relative species change of scenario 1 (SSP1)
 prop_s <- ebv_properties(filepath = path,
                          metric = 2,
                          scenario = 1)
 
 # How is the relative species change calculated (metric 2)?
 prop_s@metric
+#if you don't define the metric in the properties:
+prop@metric
 
 # What timesteps (dates) does the dataset cover?
 prop@temporal$dates
@@ -42,7 +45,7 @@ prop@spatial$dimensions
 prop@general$entity_names
 
 # 4. Read data ----
-# Read the data of the species richness for all dates for the Regional Rivalry scenario
+# Read the data of the species richness for all dates for the SSP3 scenario
 # The function returns a terra SpatRast by default. Change the type argument to change.
 data <- ebv_read(filepath = path,
                  metric = 'Species richness, S', #alternative value: 1
@@ -58,3 +61,9 @@ terra::plot(data[[2]])
 # Clean up ----
 file.remove(path)
 file.remove(paste0(str_remove(path, '.nc'), '_metadata.json'))
+
+# 5. Do by yourselves - choose freely from: ----
+# Download another dataset, e.g. ID 82 and re-run code, adapt arguments (e.g. there is no scenario in this dataset but it holds taxonomic info)
+# Check all properties that are covered by ebv_properties()
+# Test the different ways of defining scenario, metric, entity and timestep ('alternative')
+# Check out ebv_map() or ebv_trend()
