98fd4945d97c617cfd75acc4e986ef8d9e1c126b
Luise Quoß <luise.quoss@posteo.de>
2024-10-10 11:15:37 +0200

    add feedback

--- code/02_create_ebvcube.R
+++ code/02_create_ebvcube.R
@@ -5,12 +5,13 @@
 # Author: Lina M. Estupinan-Suarez
 # Institution: German Centre for Integrative Biodiversity Research
 
-# Load libraries
+# Load packages
 library(ebvcube)
 library(terra)
 
-# Path to EBV metadata as JSON 
-json <- "./metadata/json/test.json" 
+# 1. Define paths ----
+# Path to EBV metadata as JSON
+json <- "./metadata/json/test.json"
 # json_data <- fromJSON(file=json) # in case you want to read the jsonfile
 
 # Path to Tiffs
@@ -19,12 +20,13 @@ pathin <- "./data/input/tif/"
 # Path to the EBVCube output
 nc <- "./data/output/datacubes/nc/ebvcube_test.nc"
 
-# Plot data
+# 2. Plot data ----
 tifs <- c('entity_1.tif', 'entity_2.tif', 'entity_3.tif')
 tif_paths <- file.path("./data/input/tif", tifs)
 rin <- rast(tif_paths[1])
 plot(rin)
 
+# 3. Create EBVCube netCDF ----
 # Defining the fillvalue - optional
 fv <- -3.4e+38
 
@@ -32,40 +34,45 @@ fv <- -3.4e+38
 extent <- c(-19, 55, -36, 40)
 epsg <- 4326
 
+#Define entity-names
+entities <- c('a', 'b', 'c')
+
 # Create the netCDF structure (no data yet)
 ebv_create(jsonpath = json,
         outputpath = nc,
-        entities = entities,
-        epsg = epsg, 
-        extent = extent, 
+        entities = entities, #alternative: csv (see help-page)
+        epsg = epsg,
+        extent = extent,
         resolution = c(1, 1),
         fillvalue = fv,
         overwrite = TRUE,
-        verbose = FALSE)
+        verbose = FALSE #set to TRUE to see additional messages
+        )
 
-
-# Check paths to data cubes
-ebv_datacubepaths(nc)
-
-# Check metadata
-ebv_properties(nc, metric=3)
-
-# Add data
-# Get entity names
-entity_names <- ebv_properties(nc, verbose=F)@general$entity_names
 
 # Check out the (still empty) datacubes that are available
-dc.new <- ebv_datacubepaths(nc, verbose=FALSE)
-print(dc.new)
+dc_new <- ebv_datacubepaths(nc, verbose=FALSE)
+print(dc_new)
 
-# Add the data
+# Check metadata
+prop <- ebv_properties(nc, metric=3, verbose=FALSE)
+print(prop@spatial)
+
+# 4. Add data to the netCDF ----
+# Get entity names
+entity_names <- prop@general$entity_names
+print(entity_names)
+
+# Add the data to metric 1
+# Note: to fill all cubes, loop though all metrics (left out here)
 entity <- 1
 for (tif in tif_paths){
-  ebv_add_data(filepath_nc = nc, 
-               datacubepath = dc.new[entity,1],
+  ebv_add_data(filepath_nc = nc,
+               metric = 1,
                entity = entity,
                timestep = 1:3,
-               data = tif,
-               band = 1:3)
-               entity <- entity + 1
+               data = tif #alternative value: give data as an array directly
+               band = 1:3 #refers to the bands in the Tiff -> add all 3 timesteps at once
+               )
+  entity <- entity + 1
 }
