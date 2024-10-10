3d098753c2e4f2d52d3070b0d9e64b5e4c9feba4
Luise Quoß <luise.quoss@posteo.de>
2024-10-10 14:24:09 +0200

    change json dependency

--- code/02_create_ebvcube.R
+++ code/02_create_ebvcube.R
@@ -8,13 +8,12 @@
 # Load packages
 library(ebvcube)
 library(terra)
-library(rjson)
-
+library(jsonlite)
 
 # 1. Define paths ----
 # Path to EBV metadata as JSON
-json <- "data/input/json/test.json" #if error: add one dot and try again
-json_data <- fromJSON(file=json) # in case you want to read the jsonfile
+json <- "./data/input/json/test.json" #if error: add one dot and try again
+json_data <- fromJSON(txt=json) # in case you want to read the jsonfile
 
 # Path to Tiffs
 pathin <- "./data/input/tif/" #if error: add one dot and try again
@@ -69,7 +68,7 @@ print(entity_names)
 entity <- 1
 for (tif in tif_paths){ #in this example each metric is one tif
   ebv_add_data(filepath_nc = nc,
-               metric = 1, 
+               metric = 1,
                entity = entity,
                timestep = 1:3,
                data = tif, #alternative value: give data as an array directly
